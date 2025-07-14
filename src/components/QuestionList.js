64389d05c60aa461d76ce72f284737f88ac28bea
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-07-14 17:57:45 +0300

    DELETE http verb specify id

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import QuestionForm from "./QuestionForm";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
   const [questions, setQuestions]=useState([]);
@@ -18,16 +19,29 @@ function QuestionList() {
     console.log("updates");
     setQuestions([...questions, newQuestion]);
   }
+
+  function handleDeleteItem(id){
+    fetch(`http://localhost:4000/questions/${id}`,{
+      method: "DELETE"
+    })
+    .then(r=>r.json())
+    .then(data=>{
+      console.log(data);
+      const updatedQuestions=questions.filter((question)=>{
+        return question.id!==id;
+      })
+      setQuestions(updatedQuestions)
+    })
+  }
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{questions.map((question)=>{
-        return (<li key={question.id}>{question.prompt}
-       </li>
-      )
-    })}
+      <ul>{questions.map((question)=>(
+        < QuestionItem key={question.id} question={question} onDeleteQuestion={handleDeleteItem}/>
+    ))}
        </ul>
        <QuestionForm onUpdateQuestion={handleSubmitQuestion} />
+      
     </section>
   );
 }
